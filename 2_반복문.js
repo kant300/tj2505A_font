8b6768f3de0db14daf4f786261601a32f1dc0164
kant300 <p.o.i.u.0987552@gmail.com>
2025-06-13 12:45:23 +0900

    1

---
+++ 2_반복문.js
@@ -0,0 +1,105 @@
+
+/*문제 6: 배열 요소의 합계와 평균 구하기
+다음 학생들의 점수가 담긴 배열이 있습니다. for 반복문을 사용하여 
+모든 점수의 합계와 평균을 계산하여 콘솔에 출력하시오.
+let scores = [85, 92, 78, 65, 95];
+*/
+// 배열내 모든 점수 조회 : 0부터 마지막 인덱스까지 조회
+//for(let index = 0 ; index <= 마지막인덱스 ; index++){ }
+let scores1 = [85, 92, 78, 65, 95];
+let sum = 0;    //누적 총합계를 구해야 하므로 for 밖에 선언
+for( let index = 0 ; index <= scores1.length - 1 ; index++ ){
+    // index는 0부터 마지막 인덱스 이하까지 1씩 증가 반복
+    // let sum = 0; 여기에 합계 변수를 선언하면 안된다 (초기화됨 반복): 합계변수는 누적해야 하므로 선언을 반복하면 안됨.
+    sum += scores1[index];    //배열내 index번째의 요소 값을 더한다.
+}   //for end
+console.log( ` 총점 : ${ sum } , 평균 : ${ sum/scores1.length }` );
+
+/*
+문제 7: 특정 조건에서 반복문 탈출하기 (break)
+점수 배열에서 100점 만점자가 처음 나타나면, 그 학생의 점수를 출력하고 
+반복문을 즉시 종료하는 프로그램을 작성하시오.
+let scores = [77, 82, 100, 54, 96];
+출력 예시: 100점 만점자를 찾았습니다!
+*/
+// 순회/조회/검색
+let scores = [77, 82, 100, 54, 96];
+for( let index = 0 ; index <= scores.length - 1 ; index++ ) {
+    let value = scores[index];  // index번째 요소값 호출
+    if ( value == 100 ) {   // 만야게 해당값이 100(만점) 이면
+        console.log( `${value}점 만점자를 찾았습니다.!` );
+        break;  //가장 가까운 for{} 탈출/종료
+    }   //if end
+}   //for end
+//
+
+/*
+문제 8: 특정 조건 건너뛰기 (continue)
+점수 배열에서 60점 미만(과락)인 점수는 건너뛰고, 
+60점 이상인 점수만 콘솔에 출력하는 프로그램을 작성하시오.
+let scores = [90, 45, 72, 88, 59, 100];
+*/
+let scores2 = [90, 45, 72, 88, 59, 100];
+for( let index = 0 ; index <= scores2.length - 1 ; index++ ){
+    let value = scores2[index]; //index 번째의 요소값 호출
+    if( value < 60 ) {  continue; } // 가장 가까운 반복문의 증감식으로 이동
+        console.log( value );
+    }   //for end
+
+
+/*
+문제 9: 배열에서 특정 값의 개수 세기 (누적합계)
+다음 배열에서 'A'형 혈액형을 가진 사람이 몇 명인지 for 반복문을 통해 세고, 
+그 수를 콘솔에 출력하시오.  indexOf
+let bloodTypes = ['A', 'B', 'O', 'AB', 'A', 'B', 'A'];
+*/
+
+let bloodTypes = ['A', 'B', 'O', 'AB', 'A', 'B', 'A'];
+let countA = 0  //A형 개수를 저장하는 변수
+for( let index = 0 ; index <= bloodTypes.length - 1 ; index++ ){
+    let blood = bloodTypes[index]; //index번째의 혈액형 호출
+    if( blood == 'A' ){     //만약에 혈액형이 A이면
+        countA++;   // vs countA += 1 vs countA = countA + 1
+    }
+    console.log( `A형 개수 : ${ countA }` );
+}
+
+
+/*
+문제 10: 학생 점수 시각화하기 
+주어진 학생 이름과 점수 배열을 이용하여,  document.write
+각 학생의 점수를 동그라미(●, ○)로 시각화하여 HTML에 출력하는 프로그램을 작성하시오.
+(1). 초기 데이터
+학생 이름과 점수는 두 배열의 동일한 인덱스를 사용합니다.
+let nameArray = ['유재석', '강호동', '신동엽'];
+let scores3 = [92, 86, 72];
+(2). 구현 조건
+for 반복문을 사용하여 모든 학생의 데이터를 순회합니다.
+점수를 100점 만점으로 환산하여 시각화합니다.
+점수의 십의 자리 숫자만큼 꽉 찬 동그라미(●)를, **나머지(10 - 십의 자리 수)**만큼 빈 동그라미(○)를 출력합니다.
+예시: 점수가 92점이면 십의 자리는 9이므로, ●●●●●●●●●○ 와 같이 표현합니다.
+HTML에 학생 이름과 변환된 점수 시각화를 한 줄씩 출력합니다.
+(3). 출력 예시 (HTML)
+유재석 ●●●●●●●●●○
+강호동 ●●●●●●●●○○
+신동엽 ●●●●●●●○○○
+*/
+let nameArray = ['유재석', '강호동', '신동엽'];
+let scores3 = [92, 86, 72];
+let output = ""
+
+for( let index = 0 ; index <= nameArray.length - 1 ; index++ ){
+    //이름 출력
+    let name = nameArray[index];
+    output += `<div> ${ name }`;     //HTML 문자열추가
+    //점수출력
+    let score4= scores3[index];
+    let black = parseInt( score4 / 10 ); 
+    for( let circle = 1 ; circle <= 10 ; circle++){
+        if( circle <= black ) { output += '<span>●</span>'}
+        else { output  += '<span>○</span>' }
+    }
+    // -- 줄바꿈
+    output += `</div>`
+}   //for end
+document.write( output );
